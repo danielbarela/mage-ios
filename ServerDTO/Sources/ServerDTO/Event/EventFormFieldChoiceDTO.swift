56306cd419f6beccf2862b011e6cb0f3f3ea36b9
Daniel Barela <daniel.barela@gmail.com>
2026-09-03 07:56:50 -0600

    removing unused structs

--- ServerDTO/Sources/ServerDTO/Event/EventFormFieldChoiceDTO.swift
+++
@@ -1,7 +0,0 @@
-
-// periphery:ignore - DTO is meant to reflect the server
-public struct EventFormFieldChoiceDTO: Codable, Sendable {
-    public let id: Int
-    public let title: String?
-    public let value: Int
-}
